cc11a23bfb2f0f3e12113ce315cbf651aa9f434d
Daria Buinovskaya <dashk0oo@mail.ru>
2017-04-04 19:10:48 +0500

    Исправления кода.

--- text_task/Program.cs
+++ text_task/Program.cs
@@ -7,49 +7,38 @@ using System.Threading.Tasks;
 namespace text_task
 {
     class Program
-    {
-        static void Main(string[] args)
+
+    { 
+        static bool YnikalSymb( string text, char symb)
         {
-            string text = System.IO.File.ReadAllText(@"C:\Users\buino_000\Desktop\new2.txt");
-            Console.WriteLine(text);
-
-            //Cчитаем количество слов в исходном тексте
-
-            string[] textArray = text.Split(new char[] { ' ' });
-            Console.WriteLine("Колличество слов в тексте: " + textArray.Length);
-
-            //Ищем наиболее часто встречаемую букву в тексте
-            int count = 0;
-            char letter = ' '; // это будет наиболее часто встречающийся символ
-            for (char c = 'а'; c < 'я'; c++) // цикл по  алфавиту
-            {
-                int tmp = 0; // количество повторений в тексте текущей буквы
-                for (int i = 0; i < text.Length; i++) // цикл по всему тексту для подсчета количества текущего символа (char c - текущий символ)
-                    if (text[i] == c) // если нашли текущий символ, инкрементим счетчик
-                        tmp++;
-                if (tmp > count)
-                {
-                    count = tmp;
-                    letter = c;
-                }
-            }
-
-            Console.WriteLine("Чаще всего встречается буква " + letter + ", ее в тексте " + count + " штук.");
-
-            //Найдем сколько всего букв в исходном тексте
-
-            int lettersIntext = 0;
             for (int i = 0; i < text.Length; i++)
             {
-                if (text.Substring(i, 1) != " ")
-                    lettersIntext++;
+                if (symb == text[i])
+                    return false;
             }
-            Console.WriteLine("Количество букв равно {0}", lettersIntext);
+            return true;
+        }
+        static void Main(string[] args)
+        {
 
-            //Найдем процентное отношение частовстречаемой буквы 
-            double procentletter = 0;
-            procentletter = (count * 100) / lettersIntext;
-            Console.WriteLine("Процентное отношение частовстречаемой буквы к остальным равно примерно " + procentletter + "%");
+            string  a= System.IO.File.ReadAllText(@"C:\Users\buino_000\Desktop\new 2.txt");
+            Console.WriteLine(a);
+            a = a.Trim(new char[] { ',', '.', '-', '"', ')', '(' });
+            //Cчитаем количество слов в исходном тексте
+
+            string[] textArray = a.Split(new char[] { ' ' });
+            Console.WriteLine("Колличество слов в тексте: " + textArray.Length);
+            
+            //Уникальных букв в первом слове
+            string text=" ";
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (YnikalSymb(text,a[i]))
+                text = text + a[i];
+            }
+            Console.WriteLine(text);
+
+
             Console.ReadKey();
 
         }
